b3171eaf34a37e76b80b940ac61143f51671afb8
bhuvan2002 <bhuvanumesh123@gmail.com>
2024-04-12 12:47:11 +0530

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,12 @@
+from Spinal_Cord_disease_detection import logger
+from Spinal_Cord_disease_detection.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
